6432f798b86260478faab5e296a8fbc7d6efdebe
wltnthss <60785586+wltnthss@users.noreply.github.com>
2022-03-13 01:09:43 +0900

    server.port = 8000 설정 & SpringSecurity 주석

--- Shoppingmall/src/main/java/com/shoppingmall/ShoppingmallApplication.java
+++ Shoppingmall/src/main/java/com/shoppingmall/ShoppingmallApplication.java
@@ -2,7 +2,10 @@ package com.shoppingmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ShoppingmallApplication {
 
@@ -10,4 +13,8 @@ public class ShoppingmallApplication {
 		SpringApplication.run(ShoppingmallApplication.class, args);
 	}
 
+@GetMapping(value = "/")
+public String HelloWorld(){
+	return "Hello World";
+}
 }
