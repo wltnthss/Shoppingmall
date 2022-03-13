2fa85ee4e29c94f0e5d88ac24fbc739c35dd0767
wltnthss <60785586+wltnthss@users.noreply.github.com>
2022-03-13 17:49:44 +0900

    lombok test && server.port = 8070

---
+++ Shoppingmall/src/main/java/com/shoppingmall/UserDto.java
@@ -0,0 +1,14 @@
+package com.shoppingmall;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+// Lombok Test
+@Getter
+@Setter
+@ToString
+public class UserDto {
+    private String name;
+    private Integer age;
+}
