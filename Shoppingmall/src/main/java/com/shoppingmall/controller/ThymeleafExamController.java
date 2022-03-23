a68c8978b20139bb7b834554cdb8fd950aa4f52d
wltnthss <60785586+wltnthss@users.noreply.github.com>
2022-03-23 19:24:15 +0900

    thymeleaf th:each 추가

--- Shoppingmall/src/main/java/com/shoppingmall/controller/ThymeleafExamController.java
+++ Shoppingmall/src/main/java/com/shoppingmall/controller/ThymeleafExamController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")   // url에 thymeleaf 경로 요청이 오면 Controller가 역할을 위임
 public class ThymeleafExamController {
     @GetMapping(value = "/exam01")
-    public String thymeleafExample01(Model model){
+    public String thymeleafExample01(Model model) {
         model.addAttribute("data", "타임리프 예제 test");
         return "thymeleafEx/thymeleafEx01";     // templates 폴더 기준으로 뷰의 위치와 이름(thymeleafEx01.html) 반환
     }
 
     @GetMapping(value = "/exam02")
-    public String thymeleafExample02(Model model){
+    public String thymeleafExample02(Model model) {
         ProductDto productDto = new ProductDto();
         productDto.setProductName("테스트 상품1");
         productDto.setDescription("상품 설명");
@@ -27,5 +29,23 @@ public class ThymeleafExamController {
 
         model.addAttribute("productDto", productDto);
         return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping(value = "/exam03")
+    public String thymeleafExample03(Model model) {
+        List<ProductDto> productDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+            ProductDto productDto = new ProductDto();
+            productDto.setProductName("테스트 상품1" + i);
+            productDto.setDescription("상품 설명" + i);
+            productDto.setPrice(10000 * i);
+            productDto.setCreatedTime(LocalDateTime.now());
+
+            productDtoList.add(productDto);
+        }
+
+        model.addAttribute("productDtoList", productDtoList);
+        return "thymeleafEx/thymeleafEx03";
     }
 }
