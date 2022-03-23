551a820decfc6a6bcb5866cf231336f11dde858d
noru <qkr2633911@naver.com>
2022-03-24 01:32:23 +0900

    notice 테이블 view 생성 test

--- Shoppingmall/src/main/java/com/shoppingmall/repository/NoticeRepository.java
+++ Shoppingmall/src/main/java/com/shoppingmall/repository/NoticeRepository.java
@@ -1,7 +1,13 @@
 package com.shoppingmall.repository;
 
 import com.shoppingmall.entity.Notice;
+import com.shoppingmall.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface NoticeRepository extends JpaRepository<Notice,Long> {
+import java.util.List;
+
+public interface NoticeRepository extends JpaRepository<Notice,Long>, QuerydslPredicateExecutor<Notice> {
+   List<Notice> findByNoticeTitle(String NoticeTitle);
+   List<Notice> findByNoticeNum(String NoticeNum);
 }
