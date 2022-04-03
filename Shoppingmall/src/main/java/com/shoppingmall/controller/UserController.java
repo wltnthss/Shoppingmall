1cd10835c7f246ee87037c6cb73596349e1b8ef3
noru <qkr2633911@naver.com>
2022-04-03 21:34:13 +0900

    spring-boot-starter-validation => 회원가입 값 넘기기

--- Shoppingmall/src/main/java/com/shoppingmall/controller/UserController.java
+++ Shoppingmall/src/main/java/com/shoppingmall/controller/UserController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/users")
 @Controller
@@ -31,5 +34,19 @@ public class UserController {
         userService.saveUser(user);
         return "redirect:/";
 
+    }
+
+    public String newMember(@Valid UserFormDto userFormDto, BindingResult bindingResult, Model model){
+        if(bindingResult.hasErrors()){
+            return "user/userForm";
+        }
+        try{
+            User user = User.createUser(userFormDto,passwordEncoder);
+            userService.saveUser(user);
+        }catch (IllegalAccessError e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "user/userForm";
+        }
+        return "redirec:/";
     }
 }
