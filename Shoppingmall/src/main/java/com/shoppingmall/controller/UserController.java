53579b3b177cfacc483e5bf597fe515b3ace0554
wltnthss <60785586+wltnthss@users.noreply.github.com>
2022-04-04 01:28:28 +0900

    spring validator 추가

--- Shoppingmall/src/main/java/com/shoppingmall/controller/UserController.java
+++ Shoppingmall/src/main/java/com/shoppingmall/controller/UserController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/users")
 @Controller
@@ -26,10 +29,19 @@ public class UserController {
     }
 
     @PostMapping(value = "/new")
-    public String userForm(UserFormDto userFormDto){
-        User user = User.createUser(userFormDto, passwordEncoder);
-        userService.saveUser(user);
-        return "redirect:/";
+    public String userForm(@Valid UserFormDto userFormDto, BindingResult bindingResult, Model model){
 
+        if(bindingResult.hasErrors()){
+            return "user/userForm";
+        }
+
+        try {
+            User user = User.createUser(userFormDto, passwordEncoder);
+            userService.saveUser(user);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "user/userForm";
+        }
+        return "redirect:/";
     }
 }
